d448507ff17f6974f456094f6e2bec46f1022d9e
ChoiJiyeon <dkcl.8642@gmail.com>
2024-11-12 16:54:00 +0900

    javascript_4

---
+++ js/ex23.js
@@ -0,0 +1,12 @@
+const obj = {
+    t01 : () => {
+        console.log("t01실행")
+    },
+    name : "홍길동"
+}
+const test01 = () => {
+    console.log("test01")
+}
+function test02(){
+    console.log("test02")
+}
